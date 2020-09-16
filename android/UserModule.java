7af096dfe556474b9b4dcc92a5946fb126494d5c
MalliMTC <Your>
2020-09-16 11:09:05 +0400

    Fixed the issue

--- android/UserModule.java
+++ android/UserModule.java
@@ -5,7 +5,7 @@ public class UserModule extends BankApp {
     public getUserModuleInfo(){
         return "ITS DB OPeration"
 
-	There is an Error 
+	Issue resolved ; //Bader added this code
     }
 
 }
